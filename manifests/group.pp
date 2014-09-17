385ca85de68ed0dd0b77e5ce1d6fe41dd2194974
Justice London <jlondon@syrussystems.com>
2014-09-16 18:09:23 -0700

    Added acceptable exit codes for exec statement

--- manifests/group.pp
+++ manifests/group.pp
@@ -71,6 +71,7 @@ define csync2::group (
     path        => ['/sbin','/bin','/usr/bin','/usr/sbin'],
     timeout     => 300,
     refreshonly => true,
+    returns     => ['0','2'],
     require     => Concat[$::csync2::params::configfile],
     notify      => Exec['csync2_sync_nodes'],
   }
